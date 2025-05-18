d957de1f64f521bb92ea18299fd95a9fa192631e
Marios Caushi <97966740+Marioscaushi@users.noreply.github.com>
2025-05-18 13:20:28 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -7,16 +7,6 @@ from itsdangerous import URLSafeTimedSerializer
 import pymysql
 import requests
 import os
-import psycopg2
-
-conn = psycopg2.connect(
-    host=os.getenv("DB_HOST"),
-    port=os.getenv("DB_PORT"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    dbname=os.getenv("DB_NAME")
-)
-
 
 # Configuration
 app = Flask(__name__)
